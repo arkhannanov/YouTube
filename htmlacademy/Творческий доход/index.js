14e3018d51816e0eb098cf7b109f501aedf0cd15
Artur Khannanov <arkhannanov@gmail.com>
2024-03-05 12:41:07 +0300

    Задача Творческий доход

---
+++ htmlacademy/Творческий доход/index.js
@@ -0,0 +1,7 @@
+if (price < 1000) discountedPrice = price
+
+if (price >= 1000 && price < 3000 ) discountedPrice = price * 0.95
+
+if (price >= 3000 && price < 5000 ) discountedPrice = price * 0.90
+
+if (price >= 5000) discountedPrice = price * 0.85
